257fade009e3fbdd2c161da55d2ebca56f0fd04a
Andrew Laski <alaski@gmail.com>
2012-04-11 12:42:00 -0400

    Updates for Linux Mint 12 LXDE edition

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -1,13 +1,14 @@
 import qualified Data.Map as M
+
 import System.IO
 import Graphics.X11.ExtraTypes.XF86
 import XMonad
-import XMonad.Config.Xfce
+import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.SimplestFloat
-import XMonad.Util.EZConfig
+import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Run
 
 
@@ -19,25 +20,24 @@ myWorkspaces = ["1:main","2:web","3","4:pandora","5:float","6","7","8","9"]
 myManageHook = composeAll
     [ className =? "MPlayer"        --> doFloat
     , className =? "Gimp-2.6"       --> doFloat
-    , className =? "Xfce4-appfinder"--> doFloat
-    , className =? "Xfrun4"         --> doFloat
     , className =? "Google-chrome"  --> doShift "2"
     , resource  =? "www.pandora.com"  --> doShift "4:pandora"
-    , title     =? "Convergence"  --> doShift "5:float"
     , resource  =? "desktop_window" --> doIgnore
     , resource  =? "kdesktop"       --> doIgnore ]
 
 main = do
   xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
-  xmonad $ xfceConfig 
+  xmonad $ desktopConfig
     { terminal   = myTerminal
     , modMask    = mod4Mask
     , workspaces = myWorkspaces
-    , manageHook = manageDocks <+> myManageHook <+> manageHook xfceConfig
-    , layoutHook = avoidStruts $ onWorkspace "5:float" simplestFloat $ layoutHook xfceConfig
+    , manageHook = manageDocks <+> myManageHook <+> manageHook desktopConfig
+    , layoutHook = avoidStruts $ onWorkspace "5:float" simplestFloat $ layoutHook desktopConfig
     , logHook = dynamicLogWithPP $ xmobarPP
         { ppOutput = hPutStrLn xmproc
         , ppTitle = xmobarColor "green" "" . shorten 50
         , ppLayout = const "" -- to disable the layout info on xmobar
         }
-    }
+    } `additionalKeys`
+    [ ((mod4Mask, xK_p ), spawn "dmenu_run")
+    ]
