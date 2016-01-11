423cad7348b62a700bf37a69d59270ada449f2e9
Andrew Laski <andrew.laski@rackspace.com>
2016-01-11 11:06:18 -0500

    Update xrandr script for new monitor

--- bin/bin.symlink/xrandr_multi.sh
+++ bin/bin.symlink/xrandr_multi.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-xrandr | grep VGA1 | grep " connected "
+xrandr | grep DP1 | grep " connected "
 if [ $? -eq 0 ]; then
-    # External monitor is connected
-    xrandr --output LVDS1 --mode 1280x800 --output VGA1 --mode 1440x900 --left-of LVDS1
+    # External monitor is connected on displayport.
+    xrandr --output LVDS1 --mode 1280x800 --output DP1 --mode 1920x1200 --left-of LVDS1
     if [ $? -ne 0 ]; then
         # Something went wrong.  Autoconfigure internal monitor and disable external one
-        xrandr --output LVDS1 --mode auto --output VGA1 --off
+        xrandr --output LVDS1 --mode auto --output DP1 --off
     fi
 else
     # External monitor is not connected
-    xrandr --output LVDS1 --mode 1280x800 --output VGA1 --off
+    xrandr --output LVDS1 --mode 1280x800 --output DP1 --off
 fi
